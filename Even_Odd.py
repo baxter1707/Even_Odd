db942e423f33bc711a3319e8bba0c46683b68faf
Michael Baxter <msb@Michaels-MBP.localdomain>
2017-11-15 13:49:14 -0600

    EVen/Odd Exercise

---
+++ Even_Odd.py
@@ -0,0 +1,10 @@
+number = int(raw_input("Please enter your number. "))
+
+if number%2 == 0:
+    print("Your number is even!")
+
+elif number%2 != 0:
+    print("Your number is odd!")
+
+##if number != int
+    ##print("Thats not even an integer!")
