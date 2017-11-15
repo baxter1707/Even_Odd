464c5a063c61730d326deab65b99c656fddc07fc
Michael Baxter <msb@Michaels-MBP.localdomain>
2017-11-15 15:20:32 -0600

    New function updates

--- Even_Odd.py
+++ Even_Odd.py
@@ -1,10 +1,15 @@
+import Evencheck as chk
+
 number = int(raw_input("Please enter your number. "))
 
-if number%2 == 0:
-    print("Your number is even!")
+chk.checkForEvenOdd(number)
 
-elif number%2 != 0:
-    print("Your number is odd!")
+
+#if number%2 == 0:
+#    print("Your number is even!")
+
+#elif number%2 != 0:
+#    print("Your number is odd!")
 
 ##if number != int
     ##print("Thats not even an integer!")
---
+++ Evencheck.py
@@ -0,0 +1,7 @@
+
+def checkForEvenOdd(number):
+    if number%2 == 0:
+        print("Your number is even!")
+
+    elif number%2 != 0:
+        print("Your number is odd!")
